eaed92154a27dfb85e32253152a211591429cdd2
Lateralus680 <Lateralus680@gmail.com>
2022-01-05 01:22:58 +0300

    add all get tests

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -6,35 +6,35 @@
 //});
 //});
 
-// it('Search by id', () => {
-//     cy.visit('https://www.facebook.com/')
-//     cy.get('#email')
-// }); 
+it('Search by id', () => {
+    cy.visit('https://www.facebook.com/')
+    cy.get('#email')
+}); 
 
-// it('Search by class', () => {
-//     cy.visit('https://docs.cypress.io/api/commands/get')
-//     cy.get('.DocSearch.DocSearch-Button')
-// }); 
+it('Search by class', () => {
+    cy.visit('https://docs.cypress.io/api/commands/get')
+    cy.get('.DocSearch.DocSearch-Button')
+}); 
 
-// it('Search by tag', () => {
-//     cy.visit('https://docs.cypress.io/api/commands/get')
-//     cy.get('nav')
-// }); 
+it('Search by tag', () => {
+    cy.visit('https://docs.cypress.io/api/commands/get')
+    cy.get('nav')
+}); 
 
-// it('Search by tag value', () => {
-//     cy.visit('https://www.facebook.com/')
-//     cy.get('[name="pass"]')
-// }); 
+it('Search by tag value', () => {
+    cy.visit('https://www.facebook.com/')
+    cy.get('[name="pass"]')
+}); 
 
-// it('Search by different types', () => {
-//     cy.visit('https://docs.cypress.io/api/commands/get')
-//     cy.get('img[alt="Cypress Docs Logo"]')
-// }); 
+it('Search by different types', () => {
+    cy.visit('https://docs.cypress.io/api/commands/get')
+    cy.get('img[alt="Cypress Docs Logo"]')
+}); 
 
-// it('Search by contains name', () => {
-//     cy.visit('http://next.privat24.ua/')
-//     cy.get('*[class^="card"]')
-// }); 
+it('Search by contains name', () => {
+    cy.visit('http://next.privat24.ua/')
+    cy.get('*[class^="card"]')
+}); 
 
 it('Using Get with Find and Eq', () => {
     cy.visit('http://next.privat24.ua/deposit/open')
@@ -46,5 +46,3 @@ it.only('Using Get with Find and Eq 2', () => {
     cy.visit('https://docs.cypress.io/api/commands/get#Examples')
     cy.get('main').find('div').find('nav').find('li').find('a').eq('4')
     });
-
-    // comment
