c86673fdd790d28d45b8602e215d8948ea48607f
Lateralus680 <Lateralus680@gmail.com>
2022-01-05 01:28:39 +0300

    delete all tests for master

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,50 +1,6 @@
 /// <reference types="Cypress" />
 
-//describe("Mobile phone replenishment", () => {
-//context("Replenishment less than the allowed amount", () => {
-//    it("Show error: Minimum amount of the replenishment is 1 UAH", () => {});
-//});
-//});
 
-// it('Search by id', () => {
-//     cy.visit('https://www.facebook.com/')
-//     cy.get('#email')
-// }); 
 
-// it('Search by class', () => {
-//     cy.visit('https://docs.cypress.io/api/commands/get')
-//     cy.get('.DocSearch.DocSearch-Button')
-// }); 
 
-// it('Search by tag', () => {
-//     cy.visit('https://docs.cypress.io/api/commands/get')
-//     cy.get('nav')
-// }); 
 
-// it('Search by tag value', () => {
-//     cy.visit('https://www.facebook.com/')
-//     cy.get('[name="pass"]')
-// }); 
-
-// it('Search by different types', () => {
-//     cy.visit('https://docs.cypress.io/api/commands/get')
-//     cy.get('img[alt="Cypress Docs Logo"]')
-// }); 
-
-// it('Search by contains name', () => {
-//     cy.visit('http://next.privat24.ua/')
-//     cy.get('*[class^="card"]')
-// }); 
-
-it('Using Get with Find and Eq', () => {
-    cy.visit('http://next.privat24.ua/deposit/open')
-    cy.get('tbody').find('td').find('div').find('button').eq('0')
-    });
-
-it.only('Using Get with Find and Eq 2', () => {
-    cy.viewport(1800, 700)
-    cy.visit('https://docs.cypress.io/api/commands/get#Examples')
-    cy.get('main').find('div').find('nav').find('li').find('a').eq('4')
-    });
-
-    // comment
