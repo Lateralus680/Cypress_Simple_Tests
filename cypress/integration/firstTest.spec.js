5e6a621d3d24fbd5b7891405f13c8ac7aa63dd27
Lateralus680 <Lateralus680@gmail.com>
2022-01-05 02:01:23 +0300

    add commit

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -36,12 +36,12 @@ it('Search by contains name', () => {
     cy.get('*[class^="card"]')
 }); 
 
-it('Using Get with Find and Eq', () => {
+it.only('Using Get with Find and Eq', () => {
     cy.visit('http://next.privat24.ua/deposit/open')
     cy.get('tbody').find('td').find('div').find('button').eq('0')
 });
 
-it.only('Using Get with Find and Eq 2', () => {
+it('Using Get with Find and Eq 2', () => {
     cy.viewport(1800, 700)
     cy.visit('https://docs.cypress.io/api/commands/get#Examples')
     cy.get('main').find('div').find('nav').find('li').find('a').eq('4')
