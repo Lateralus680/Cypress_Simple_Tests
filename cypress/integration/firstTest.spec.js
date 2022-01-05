7964816fb660ca0f9f13c113d15c1e4577749b07
Lateralus680 <Lateralus680@gmail.com>
2022-01-05 15:36:40 +0300

    add contains() tests

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,6 +1,21 @@
 /// <reference types="Cypress" />
 
+it('Finding with Contains first example', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('Sign in')
+});
 
+it('Finding with Contains second example', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('div', 'Sign in')
+});
 
+it('Finding with Contains third example', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('SIGN IN', {matchCase: false})
+});
 
-
+it.only('Finding with Contains and Get', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('footer').contains('Apple store')
+});
